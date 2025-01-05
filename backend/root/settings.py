4ac02da413a4e6f52790513fe8c9a1824c2cf0fb
semper <emmanuelcopeters@gmail.com>
2025-01-05 00:32:35 -0800

    changed settings

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -184,12 +184,14 @@ if RENDER_EXTERNAL_HOSTNAME:
     DATABASE_URL= config('DATABASE_URL')
     DATABASES = {'default':dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+    DATABASE_URL= config('DATABASE_URL')
+    DATABASES = {'default':dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 
 cloudinary.config( 
   	cloud_name ='dboagqxsq',
