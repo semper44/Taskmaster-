4d3cbeeb8e8291456eac9999f10301e772ad38b1
semper44 <emmanuelcopeters@gmail.com>
2026-09-19 17:55:54 +0100

    changed config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,9 +1,7 @@
-// config.js
 const ENV = {
-    // API_URL: 'https://taskmaster-xs75.onrender.com',
-    API_URL: 'http://127.0.0.1:8000'
-    // API_URL: 'http://127.0.0.1:50264/'
-    // API_URL: 'http://34.228.65.76:8000/'
-    
-  };
-  
+    API_URL:
+        window.location.hostname === 'localhost' ||
+        window.location.hostname === '127.0.0.1'
+            ? 'http://127.0.0.1:8000'
+            : 'https://taskmaster-xs75.onrender.com'
+};
